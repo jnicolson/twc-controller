0ddf62226868ec8aea52bc35bafbfe0e09659c5c
Jarl Nicolson <jarl@jmn.id.au>
2020-07-02 19:46:12 +1000

    Connect to WiFi using credentials saved in twc_private.h (not in repo)

--- twc-controller.ino
+++ twc-controller.ino
@@ -1,6 +1,27 @@
+#include <WiFi.h>
+
+#include "twc_private.h"
+
+void setupWifi(char *ssid, char *key) {
+    Serial.print("Setting up WiFi...");
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, key);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connection Failed! Rebooting...");
+        delay(5000);
+        ESP.restart();
+    }
+
+    Serial.println("Done!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup() {
     Serial.begin(115200);
     Serial.println("Booting...");
+
+    setupWifi(SSID, PASSWORD);
 
     pinMode(LED_BUILTIN, OUTPUT);
 }
