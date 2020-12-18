7bc8e27db96653016c8238a242a35f3336a64209
Jarl Nicolson <jarl@jmn.id.au>
2020-12-19 08:31:03 +1000

    Changed OTA to a class

--- ota.cpp
+++ ota.cpp
@@ -1,8 +1,28 @@
+/*
+TWC Manager for ESP32
+Copyright (C) 2020 Jarl Nicolson
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <Arduino.h>
+#include <Update.h>
+
 #include "twc.h"
 #include "ota.h"
 
-void setupOTA() {
-  Serial.print("Setting up OTA...");
+OTA::OTA() {};
+
+void OTA::Begin() {
+  Serial.print("Setting up OTA... ");
     // Port defaults to 3232
   // ArduinoOTA.setPort(3232);
 
@@ -48,7 +68,6 @@ void setupOTA() {
   Serial.println("Done!");
 }
 
-void HandleOTA() {
-    Serial.println("In handle OTA");
+void OTA::Handle() {
     ArduinoOTA.handle();
 }
--- ota.h
+++ ota.h
@@ -1,9 +1,31 @@
+/*
+TWC Manager for ESP32
+Copyright (C) 2020 Jarl Nicolson
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef OTA_H
 #define OTA_H
 
 #include <ArduinoOTA.h>
 
-void setupOTA();
-void HandleOTA();
+class OTA {
+public:
+    OTA();
+    void Begin();
+    void Handle();
+
+private:
+
+};
 
 #endif /* OTA_H */
